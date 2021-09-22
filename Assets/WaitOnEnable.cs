0bcd48ea61c9a7f5cc44a4e4c4cbb1dd6fc80bc6
dsy5983448 <dsy5983448@gmail.com>
2021-09-23 00:34:46 +0900

    尝试延迟5帧后再启动动态骨骼

---
+++ Assets/WaitOnEnable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitOnEnable : MonoBehaviour
+{
+    public Behaviour[] Components = null;
+
+    private IEnumerator Wait()
+    {
+        for (var i = 0; i < 5; i++)
+            yield return null;
+
+        foreach (var component in Components)
+            component.enabled = true;
+
+        Destroy(this);
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Wait());
+    }
+}
