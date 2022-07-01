d502f056c44a1bdc2ea9c1bc5b43d55e86b2d6fd
lijiawen <lijw412@hotmail.com>
2022-07-01 13:15:01 +0900

    设置工厂区电梯传送

---
+++ Assets/SariaAsset/Scripts/TPScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPScene : MonoBehaviour
+{
+    [Header("要去的坐标")]
+    public Transform target;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.position = target.position;
+            other.transform.rotation = target.rotation;
+        }
+    }
+}
