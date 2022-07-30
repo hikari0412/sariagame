296df051e3a5235d5274c2e1c04fc03e698fa3d2
ShiyumeMeguri <102138304+ShiyumeMeguri@users.noreply.github.com>
2022-07-30 10:09:20 +0900

    简单的敌人生成器

---
+++ Assets/SariaAsset/Scripts/EntityGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityGenerator : MonoBehaviour
+{
+    public Transform entityPrefab;
+    Transform prefabCache;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            prefabCache = GameObject.Instantiate(entityPrefab);
+
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(prefabCache);
+
+        }
+    }
+}
