b71a0b331f82c4ba6ee24906aa94b88e9972c700
ShiyumeMeguri <102138304+ShiyumeMeguri@users.noreply.github.com>
2022-08-05 05:43:31 +0900

    音量设置和退出游戏

---
+++ Assets/SariaAsset/Scripts/SimpleUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class SimpleUI : MonoBehaviour
+{
+    public AudioMixer audioMixerUI;
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    public void BGMChange(float value)
+    {
+        audioMixerUI.SetFloat("Vol_BGM", value);
+    }
+    public void MasterChange(float value)
+    {
+        audioMixerUI.SetFloat("Vol_Master", value);
+    }
+    public void SoundEffectChange(float value)
+    {
+        audioMixerUI.SetFloat("Vol_SoundEffect", value);
+    }
+    public void VoiceChange(float value)
+    {
+        audioMixerUI.SetFloat("Vol_Voice", value);
+    }
+}
